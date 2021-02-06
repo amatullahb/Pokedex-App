1e876aa4d6d763efddb70c9a50540bf0c2947555
Amatullah Brown <amatullahbrown@Amatullahs-Air.fios-router.home>
2021-02-06 13:38:41 -0500

    retrieved all necessary data from pokeapi

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios'); //or fs
+const axios = require('axios'); 
 const pokemon25 = 'https://pokeapi.co/api/v2/pokemon?offset=151&;amp;limit=25';
 const pokemon5 = 'https://pokeapi.co/api/v2/pokemon?offset=151&;amp;limit=5'; //tester to make it easier to see
-// const fs = require('fs'); //or axios
 
 class Pokemon {
     constructor(name, url) {
@@ -23,12 +22,14 @@ router.get('/', asyncHandler(async (req, res) => {
         let data  = await getPokemonData(pokemon); // why does { pokemon.id, pokemon.type } = await getPokemonData(pokemon); produce an error?
         pokemon.id = data.id;
         pokemon.type = data.type;
+        pokemon.img = data.img;
     }
+    console.log(pokedex);
     res.render('index');
 }));
 
 function asyncHandler(cb) {
-    // abstracts away try/catch
+    // abstracts away try...catch
     return async (req, res, next) => {
         try {
             await cb(req, res, next);
@@ -44,8 +45,9 @@ async function getPokedexData(url) {
 async function getPokemonData(pokemon) {
     const res = await axios.get(pokemon.url);
     const id = res.data.id;
-    const type = res.data.types[0].type.name
-    return {id, type};
+    const type = res.data.types[0].type.name;
+    const img = res.data.sprites.front_default;
+    return {id, type, img};
 }
 
 module.exports = router;
